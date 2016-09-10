a379ff0c2f7d55345b18b7654879085b2021bd35
Ruslan Kvashuk <raidho26@gmail.com>
2016-09-10 12:36:08 +0300

    Code was refactored for automaticly create row with data without depends of number of value in generated array

--- view/create_new_row.php
+++ view/create_new_row.php
@@ -12,16 +12,16 @@ for ($i = 0; $i < count($globalArray); $i++) {
 
         $j = 0;
         echo '<tr>';
-        echo '<td class="tg-s6z2" rowspan="2">' . $globalArray[$i][$j] . '</td>';
-        echo '<td class="tg-s6z2" rowspan="2">' . $globalArray[$i][$j+1] . '</td>';
-        echo $globalArray[$i][$j+2];
-        echo '<td class="tg-s6z2">' . $globalArray[$i][$j+3] . '</td>'; // состояние
-        echo '<td class="tg-s6z2">' . $globalArray[$i][$j+5] . '</td>';
+        echo '<td class="tg-s6z2" rowspan="2">' .  ++$i . '</td>';
+        echo '<td class="tg-s6z2" rowspan="2">' . $globalArray[--$i][$j] . '</td>';
+        echo $globalArray[$i][++$j];
+        echo '<td class="tg-s6z2">' . $globalArray[$i][++$j] . '</td>'; // состояние
+        echo '<td class="tg-s6z2">' . $globalArray[$i][++$j] . '</td>';
         echo '</tr>';
         echo '<tr>';
-        echo '<td class="tg-yw4l">' . $globalArray[$i][$j+4] . '</td>'; //рекомендации
-        echo '<td class="tg-yw4l">' . $globalArray[$i][$j+6] . '</td>';
+        echo '<td class="tg-yw4l">' . $globalArray[$i][++$j] . '</td>'; //рекомендации
+        echo '<td class="tg-yw4l">' . $globalArray[$i][++$j] . '</td>';
         echo '</tr>';
 }
 
-?>
+
