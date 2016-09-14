8079951bf1521c5e76bde1df9f00a9b4f3289877
Ruslan Kvashuk <raidho26@gmail.com>
2016-09-14 21:40:29 +0300

    description on first page was added, css caching was prevented

--- view/head.php
+++ view/head.php
@@ -1,10 +1,17 @@
-<?php
-echo '<!doctype html>';
-echo '<html lang="en">';
-echo '<head>';
-echo '<meta charset="UTF-8">';
-echo '<title>Test Work</title>';
-echo '<link href="./css/styles.css" rel="stylesheet">';
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Test Work</title>
 
+    <?php
+    // Init
+    define( "DEBUGGING", true ); // or false in production enviroment
+    // Functions
+    function get_cache_prevent_string( $always = true ) {
+        return (DEBUGGING || $always) ? date('_Y-m-d_H:i:s') : "";
+    }
+    ?>
+    <link rel="stylesheet" href="./css/styles.css?version=0.1<?php echo get_cache_prevent_string(); ?>">
 
-echo '</head>';
+</head>
